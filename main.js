0d9bfc51bb7932ba83687bcd7397cedde90cb4d5
j3ffh95 <j3ffh95@gmail.com>
2020-09-08 13:32:27 -0400

    Added section var and logic for when clicked

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const parent = document.querySelector(".modal-parent"),
   btn = document.querySelector("button"),
-  X = document.querySelector(".X");
+  X = document.querySelector(".X"),
+  section = document.querySelector("section");
 
 btn.addEventListener("click", appear);
+
+function appear() {
+  parent.style.display = "block";
+  section.style.filter = "blur(10px)";
+}
