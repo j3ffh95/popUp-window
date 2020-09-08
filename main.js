a5c7a1adedc870db665cf87eb57158af963550da
j3ffh95 <j3ffh95@gmail.com>
2020-09-08 13:36:36 -0400

    Logic to make the modal go away

--- main.js
+++ main.js
@@ -9,3 +9,10 @@ function appear() {
   parent.style.display = "block";
   section.style.filter = "blur(10px)";
 }
+
+X.addEventListener("click", disappear);
+
+function disappear() {
+  parent.style.display = "none";
+  section.style.filter = "blur(0)";
+}
